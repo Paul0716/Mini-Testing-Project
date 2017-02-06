ba1cf118e4d6c7faf68fb7cac803945929e7cbbe
paultaku <justlove0714@gmail.com>
2017-02-06 08:57:30 +0800

    1. 測試full calendar 相關功能

--- vue-with-calendar.io/js/vue-fullcalendar.js
+++ vue-with-calendar.io/js/vue-fullcalendar.js
@@ -4,10 +4,38 @@ Vue.directive('fullcalendar', {
 		var options = binding.value;
 		$(el).fullCalendar({
 			weekends: true,
+			locale: "zh-tw",
+			header: {
+				left: 'prev',
+				center: 'title',
+				right: 'next',
+			},
 			dayClick: function(date, jsEvent, view) {
 				console.log(date.format("Y-M-D"));
 			},
-			lang: "zh",
+			viewRender: function(view, el) {
+				console.log("viewRender.", $(el).attr("id"));
+				console.log(view.intervalStart.format("Y-M-D"));
+				// var now = new Date(); 
+				// var end = new Date();
+				// end.setMonth(now.getMonth() + 1); //Adjust as needed
+
+				// var cal_date_string = view.intervalStart.getMonth()+'/'+view.intervalStart.getFullYear();
+				// var cur_date_string = now.getMonth()+'/'+now.getFullYear();
+				// var end_date_string = end.getMonth()+'/'+end.getFullYear();
+
+				if(cal_date_string == cur_date_string) {
+					$('.fc-button-prev').addClass("fc-state-disabled");
+				} else {
+					$('.fc-button-prev').removeClass("fc-state-disabled");
+				}
+
+				if(end_date_string == cal_date_string) {
+					$('.fc-button-next').addClass("fc-state-disabled");
+				} else {
+					$('.fc-button-next').removeClass("fc-state-disabled");
+				}
+			},
 		});
 	}
 });
